5584e79c6c57b25df4da0b2cf601d801312a4f91
Karl Leswing <karl.leswing@gmail.com>
2025-12-25 11:53:40 -0500

    GOGO

--- src/Card.js
+++ src/Card.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 function Card(props) {
+    const imgSrc = props.img.startsWith('http') ? props.img : `${process.env.PUBLIC_URL}/${props.img}`;
     return (
         <div className={"cell"}>
             <div className="image-container">
-                <img alt="" src={props.img}
+                <img alt="" src={imgSrc}
                      onClick={props.onClick}
                     style={{
                         cursor: "pointer",
